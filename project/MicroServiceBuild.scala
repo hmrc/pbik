11a0a0a66678f552a949d92686e999c6345f41d3
Emilia Trendafilova <emilia.trendafilova@opencastsoftware.com>
2019-03-11 11:42:19 +0000

    DDCNLS-5415: removed unnessecary libraries

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -44,8 +44,6 @@ private object AppDependencies {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
         "uk.gov.hmrc" %% "hmrctest" % "3.6.0-play-25" % scope,
-        "org.scalatest" %% "scalatest" % "3.0.5" % scope,
-        "org.pegdown" % "pegdown" % "1.6.0" % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope,
         "org.specs2" % "specs2_2.10" % "2.3.13",
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
