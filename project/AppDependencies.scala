2f197922d3572bb40ce6479add7472831e0be678
Victor Osipov <osipovvictor1994@gmail.com>
2023-10-09 14:14:42 +0100

    DDCE-4668 - upgrade deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,37 +1,21 @@
-/*
- * Copyright 2015 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import play.sbt.PlayImport._
-import sbt._
+import play.sbt.PlayImport.*
+import sbt.*
 
 object AppDependencies {
 
-  private lazy val bootstrapPlayVersion = "7.14.0"
+  private lazy val bootstrapPlayVersion = "7.22.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "tax-year"                  % "3.0.0"
+    "uk.gov.hmrc" %% "tax-year"                  % "3.3.0"
   )
 
   val test: Seq[ModuleID]    = Seq(
-    "org.scalatest"       %% "scalatest"              % "3.2.15",
+    "org.scalatest"       %% "scalatest"              % "3.2.17",
     "uk.gov.hmrc"         %% "bootstrap-test-play-28" % bootstrapPlayVersion,
-    "org.mockito"         %% "mockito-scala"          % "1.17.12",
-    "com.vladsch.flexmark" % "flexmark-all"           % "0.62.2"
+    "org.mockito"         %% "mockito-scala"          % "1.17.27",
+    "com.vladsch.flexmark" % "flexmark-all"           % "0.64.8"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
